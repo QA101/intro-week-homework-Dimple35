1ee62834aca03fa7b892ddb3cd517a33f9a7e8ee
Dimple35 <gamzet@yahoo.com>
2017-10-05 09:14:37 -0700

    added a simple pgm

---
+++ MyFirstProgram.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		Scanner cin = new Scanner(System.in);
+	      String a = cin.next();
+	      System.out.println(a.substring(1,1));  
+	      
+	      int i = a.length();
+	      System.out.println(i);
+        /*  for (integer i; i>=0; i--) {
+        	  System.out.println(i);
+        	  System.out.println(a.substring(i,1));
+		  */ 
+		}
+
+	}
+
+
