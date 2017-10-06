677288c2b4b69fe134f633c0064597bef80a2c69
Dimple35 <gamzet@yahoo.com>
2017-10-06 09:51:39 -0700

    Update HomeWork_1.java

--- HomeWork_1.java
+++ HomeWork_1.java
@@ -4,16 +4,17 @@ public class HomeWork_1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 						
-				System.out.println("Line1 -");  
-				System.out.println("Line2 --"); 
-				System.out.println("Line3 ---");
-				System.out.println("Line4 ----");
-				System.out.println("Line5 -----");
-				System.out.println("Line6 ------"); 
-				System.out.println("Line7 -------");
-				System.out.println("Line8 --------");
-				System.out.println("Line9 ---------");
-				System.out.println("Line10 ----------");
+				System.out.println("Hello!");  
+				System.out.println("I\'m Gamze. I\'ve been living here for almost 10 years"); 
+				System.out.println("I immigrated from Turkiye to Seattle. (The original name of the country is Turkiye not Turkey)");
+				System.out.println("I\'m working as a Project Engineer at MS. My title is Project Engineer but I\'m doing app compatiblity testing");
+				System.out.println("Here are some of my hobbies:");
+				System.out.println("----------------------------"); 
+				System.out.println("Drawing");
+				System.out.println("Zumba Dancing");
+				System.out.println("Biking");
+				System.out.println("Solving puzzles and intelligent games");
+					      
 			      
 				
 	}
