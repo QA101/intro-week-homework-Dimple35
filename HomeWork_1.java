af07935453fdb46bc204949563a8db1adae3f16e
Dimple35 <gamzet@yahoo.com>
2017-10-05 18:03:44 -0700

    Write 10 lines

---
+++ HomeWork_1.java
@@ -0,0 +1,21 @@
+
+public class HomeWork_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+						
+				System.out.println("Line1 -");  
+				System.out.println("Line2 --"); 
+				System.out.println("Line3 ---");
+				System.out.println("Line4 ----");
+				System.out.println("Line5 -----");
+				System.out.println("Line6 ------"); 
+				System.out.println("Line7 -------");
+				System.out.println("Line8 --------");
+				System.out.println("Line9 ---------");
+				System.out.println("Line10 ----------");
+			      
+				
+	}
+
+}
